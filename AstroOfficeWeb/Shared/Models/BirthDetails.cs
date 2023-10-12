0ec1b914a48edc230d8f877042b11df5ba38e432
mahesh-mobileartsme <mahesh.outsourcing@mobileartsme.com>
2023-10-12 15:00:52 +0530

    saved SalaChakkar

--- AstroOfficeWeb/Shared/Models/BirthDetails.cs
+++ AstroOfficeWeb/Shared/Models/BirthDetails.cs
@@ -32,7 +32,7 @@ namespace AstroOfficeWeb.Shared.Models
         public string Brief { get; set; } = string.Empty;
         public short CmbRotate { get; set; } = 1;
         public bool ChkSahasaneLogic { get; set; }
-        public bool SalaChakkar { get; set; }
+        public bool SalaChakkar { get; set; } = true;
         public bool ChkMfal { get; set; } = false;
         public string Longtitude { get; set; } = string.Empty;
         public string Latitude { get; set; } = string.Empty;
