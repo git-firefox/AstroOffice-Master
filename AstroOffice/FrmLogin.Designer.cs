6670a943c706c6562c6451073928d0ffd9117d3f
mahesh-mobileartsme <mahesh.outsourcing@mobileartsme.com>
2023-10-17 19:20:13 +0530

    otp inprogress complated

--- AstroOffice/FrmLogin.Designer.cs
+++ AstroOffice/FrmLogin.Designer.cs
@@ -33,15 +33,23 @@
             BtnExit = new Button();
             TxtUserName = new TextBox();
             TxtPassword = new TextBox();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            textBox4 = new TextBox();
-            textBox5 = new TextBox();
-            textBox6 = new TextBox();
+            TxtNum2 = new TextBox();
+            TxtNum3 = new TextBox();
+            TxtNum6 = new TextBox();
+            TxtNum4 = new TextBox();
+            TxtNum5 = new TextBox();
+            TxtNum1 = new TextBox();
             checkBox1 = new CheckBox();
             panel1 = new Panel();
+            VerifyOtp = new Button();
+            BtnResendOtp = new Button();
+            TxtMobileNumber = new TextBox();
+            BtnSendOtp = new Button();
+            panel2 = new Panel();
+            Panel_Cred = new Panel();
             panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            Panel_Cred.SuspendLayout();
             SuspendLayout();
             // 
             // BtnAccess
@@ -52,7 +60,7 @@
             BtnAccess.FlatAppearance.BorderSize = 0;
             BtnAccess.FlatStyle = FlatStyle.Flat;
             BtnAccess.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            BtnAccess.Location = new Point(254, 17);
+            BtnAccess.Location = new Point(248, 8);
             BtnAccess.Margin = new Padding(4, 3, 4, 3);
             BtnAccess.Name = "BtnAccess";
             BtnAccess.Size = new Size(72, 31);
@@ -69,7 +77,7 @@
             BtnExit.FlatAppearance.BorderSize = 0;
             BtnExit.FlatStyle = FlatStyle.Flat;
             BtnExit.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            BtnExit.Location = new Point(350, 17);
+            BtnExit.Location = new Point(344, 8);
             BtnExit.Margin = new Padding(4, 3, 4, 3);
             BtnExit.Name = "BtnExit";
             BtnExit.Size = new Size(72, 31);
@@ -81,7 +89,7 @@
             // TxtUserName
             // 
             TxtUserName.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
-            TxtUserName.Location = new Point(16, 20);
+            TxtUserName.Location = new Point(8, 16);
             TxtUserName.Margin = new Padding(4, 6, 4, 3);
             TxtUserName.Name = "TxtUserName";
             TxtUserName.Size = new Size(95, 25);
@@ -90,77 +98,144 @@
             // TxtPassword
             // 
             TxtPassword.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
-            TxtPassword.Location = new Point(135, 20);
+            TxtPassword.Location = new Point(119, 16);
             TxtPassword.Margin = new Padding(4, 6, 4, 3);
             TxtPassword.Name = "TxtPassword";
             TxtPassword.PasswordChar = '*';
             TxtPassword.Size = new Size(95, 25);
             TxtPassword.TabIndex = 1;
+            TxtPassword.TextChanged += TxtPassword_TextChanged;
             // 
-            // textBox1
+            // TxtNum2
             // 
-            textBox1.Location = new Point(40, 16);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(24, 23);
-            textBox1.TabIndex = 4;
+            TxtNum2.Location = new Point(40, 16);
+            TxtNum2.Name = "TxtNum2";
+            TxtNum2.Size = new Size(24, 23);
+            TxtNum2.TabIndex = 4;
             // 
-            // textBox2
+            // TxtNum3
             // 
-            textBox2.Location = new Point(72, 16);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(24, 23);
-            textBox2.TabIndex = 5;
+            TxtNum3.Location = new Point(72, 16);
+            TxtNum3.Name = "TxtNum3";
+            TxtNum3.Size = new Size(24, 23);
+            TxtNum3.TabIndex = 5;
             // 
-            // textBox3
+            // TxtNum6
             // 
-            textBox3.Location = new Point(168, 16);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(24, 23);
-            textBox3.TabIndex = 6;
+            TxtNum6.Location = new Point(168, 16);
+            TxtNum6.Name = "TxtNum6";
+            TxtNum6.Size = new Size(24, 23);
+            TxtNum6.TabIndex = 6;
             // 
-            // textBox4
+            // TxtNum4
             // 
-            textBox4.Location = new Point(104, 16);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(24, 23);
-            textBox4.TabIndex = 7;
+            TxtNum4.Location = new Point(104, 16);
+            TxtNum4.Name = "TxtNum4";
+            TxtNum4.Size = new Size(24, 23);
+            TxtNum4.TabIndex = 7;
             // 
-            // textBox5
+            // TxtNum5
             // 
-            textBox5.Location = new Point(136, 16);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(24, 23);
-            textBox5.TabIndex = 7;
+            TxtNum5.Location = new Point(136, 16);
+            TxtNum5.Name = "TxtNum5";
+            TxtNum5.Size = new Size(24, 23);
+            TxtNum5.TabIndex = 7;
             // 
-            // textBox6
+            // TxtNum1
             // 
-            textBox6.Location = new Point(8, 16);
-            textBox6.Name = "textBox6";
-            textBox6.Size = new Size(24, 23);
-            textBox6.TabIndex = 8;
+            TxtNum1.Location = new Point(8, 16);
+            TxtNum1.Name = "TxtNum1";
+            TxtNum1.Size = new Size(24, 23);
+            TxtNum1.TabIndex = 8;
             // 
             // checkBox1
             // 
             checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(8, 80);
+            checkBox1.Location = new Point(160, 216);
             checkBox1.Name = "checkBox1";
             checkBox1.Size = new Size(103, 19);
             checkBox1.TabIndex = 9;
             checkBox1.Text = "Login with otp";
             checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
             // 
             // panel1
             // 
-            panel1.Controls.Add(textBox6);
-            panel1.Controls.Add(textBox1);
-            panel1.Controls.Add(textBox2);
-            panel1.Controls.Add(textBox5);
-            panel1.Controls.Add(textBox3);
-            panel1.Controls.Add(textBox4);
-            panel1.Location = new Point(120, 64);
+            panel1.Controls.Add(VerifyOtp);
+            panel1.Controls.Add(BtnResendOtp);
+            panel1.Controls.Add(TxtNum1);
+            panel1.Controls.Add(TxtNum2);
+            panel1.Controls.Add(TxtNum3);
+            panel1.Controls.Add(TxtNum5);
+            panel1.Controls.Add(TxtNum6);
+            panel1.Controls.Add(TxtNum4);
+            panel1.Location = new Point(8, 152);
             panel1.Name = "panel1";
-            panel1.Size = new Size(200, 56);
+            panel1.Size = new Size(424, 48);
             panel1.TabIndex = 10;
+            // 
+            // VerifyOtp
+            // 
+            VerifyOtp.BackColor = Color.Khaki;
+            VerifyOtp.FlatStyle = FlatStyle.Flat;
+            VerifyOtp.Location = new Point(232, 8);
+            VerifyOtp.Name = "VerifyOtp";
+            VerifyOtp.Size = new Size(88, 31);
+            VerifyOtp.TabIndex = 10;
+            VerifyOtp.Text = "Verify Otp";
+            VerifyOtp.UseVisualStyleBackColor = false;
+            VerifyOtp.Click += VerifyOtp_Click;
+            // 
+            // BtnResendOtp
+            // 
+            BtnResendOtp.BackColor = Color.Khaki;
+            BtnResendOtp.FlatStyle = FlatStyle.Flat;
+            BtnResendOtp.Location = new Point(328, 8);
+            BtnResendOtp.Name = "BtnResendOtp";
+            BtnResendOtp.Size = new Size(88, 31);
+            BtnResendOtp.TabIndex = 9;
+            BtnResendOtp.Text = "Resend Otp";
+            BtnResendOtp.UseVisualStyleBackColor = false;
+            // 
+            // TxtMobileNumber
+            // 
+            TxtMobileNumber.Location = new Point(8, 16);
+            TxtMobileNumber.Name = "TxtMobileNumber";
+            TxtMobileNumber.Size = new Size(100, 23);
+            TxtMobileNumber.TabIndex = 11;
+            TxtMobileNumber.TextChanged += TxtMobileNumber_TextChanged;
+            // 
+            // BtnSendOtp
+            // 
+            BtnSendOtp.BackColor = Color.Khaki;
+            BtnSendOtp.FlatStyle = FlatStyle.Flat;
+            BtnSendOtp.Location = new Point(344, 8);
+            BtnSendOtp.Name = "BtnSendOtp";
+            BtnSendOtp.Size = new Size(72, 31);
+            BtnSendOtp.TabIndex = 12;
+            BtnSendOtp.Text = "Send Otp";
+            BtnSendOtp.UseVisualStyleBackColor = false;
+            BtnSendOtp.Click += BtnSendOtp_Click;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(BtnSendOtp);
+            panel2.Controls.Add(TxtMobileNumber);
+            panel2.Location = new Point(8, 88);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(424, 48);
+            panel2.TabIndex = 13;
+            // 
+            // Panel_Cred
+            // 
+            Panel_Cred.Controls.Add(TxtUserName);
+            Panel_Cred.Controls.Add(TxtPassword);
+            Panel_Cred.Controls.Add(BtnAccess);
+            Panel_Cred.Controls.Add(BtnExit);
+            Panel_Cred.Location = new Point(8, 16);
+            Panel_Cred.Name = "Panel_Cred";
+            Panel_Cred.Size = new Size(424, 48);
+            Panel_Cred.TabIndex = 14;
             // 
             // FrmLogin
             // 
@@ -168,14 +243,12 @@
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(255, 128, 0);
-            ClientSize = new Size(442, 143);
+            ClientSize = new Size(442, 250);
             ControlBox = false;
+            Controls.Add(Panel_Cred);
+            Controls.Add(panel2);
             Controls.Add(panel1);
             Controls.Add(checkBox1);
-            Controls.Add(BtnExit);
-            Controls.Add(BtnAccess);
-            Controls.Add(TxtPassword);
-            Controls.Add(TxtUserName);
             FormBorderStyle = FormBorderStyle.None;
             Icon = (Icon)resources.GetObject("$this.Icon");
             Margin = new Padding(2);
@@ -185,6 +258,10 @@
             Text = "AstroScience Login";
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            Panel_Cred.ResumeLayout(false);
+            Panel_Cred.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -203,5 +280,18 @@
         private TextBox textBox6;
         private CheckBox checkBox1;
         private Panel panel1;
+        private TextBox TxtMobileNumber;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button BtnSendOtp;
+        private Panel panel2;
+        private Panel Panel_Cred;
+        private Button BtnResendOtp;
+        private Button VerifyOtp;
+        private TextBox TxtNum2;
+        private TextBox TxtNum3;
+        private TextBox TxtNum6;
+        private TextBox TxtNum4;
+        private TextBox TxtNum5;
+        private TextBox TxtNum1;
     }
 }
--- AstroOffice/FrmLogin.cs
+++ AstroOffice/FrmLogin.cs
@@ -14,15 +14,21 @@ using AstroOffice.Helper;
 using ASBAL;
 using ASModels.Astrooff;
 using Microsoft.Extensions.DependencyInjection;
+using AstroOfficeWeb.Shared.Models;
+//using System.Windows;
+
 namespace AstroOffice
 {
     public partial class FrmLogin : Form
     {
         //private readonly Loader _loader;
+        private readonly HttpClientHelper _httpClient;
+
         public FrmLogin()
         {
+            _httpClient = new HttpClientHelper("https://localhost:7085/api/");
             InitializeComponent();
-            //_loader = new Loader();
+            panel1.Visible = checkBox1.Checked;
         }
 
         private void BtnAccess_Click(object sender, EventArgs e)
@@ -68,6 +74,80 @@ namespace AstroOffice
         {
             Application.Exit();
             Application.ExitThread();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            panel1.Visible = checkBox1.Checked;
+        }
+
+        private void TxtMobileNumber_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnSendOtp_Click(object sender, EventArgs e)
+        {
+            var mobileNumber = TxtMobileNumber.Text;
+            if (string.IsNullOrEmpty(mobileNumber))
+            {
+                MessageBox.Show("Enter Mobile Number ");
+            }
+            else
+            {
+                var response = _httpClient.GetAsync<ApiResponse<string>>($"SMS/SendOtp?mobileNumber={mobileNumber}").Result;
+            }
+        }
+
+        private void VerifyOtp_Click(object sender, EventArgs e)
+        {
+            var bALUser = Program.ServiceProvider.GetRequiredService<BALUser>();
+
+            this.Hide();
+            Loader.Show();
+            AUser aUser = bALUser.UserNameSearch(this.TxtUserName.Text);
+
+            Loader.Close();
+
+            if (aUser.Sno <= 0)
+            {
+                MessageBox.Show("Invalid User name or Password");
+                this.Show();
+                return;
+            }
+
+            string otp = TxtNum1.Text + TxtNum2.Text + TxtNum3.Text + TxtNum4.Text + TxtNum5.Text + TxtNum6.Text;
+
+            if (otp == aUser.MobileOtp)
+            {
+                AUserLog aUserLog = new()
+                {
+                    Username = aUser.Username,
+                    Loginsuccess = new bool?(true),
+                    Logintime = new DateTime?(DateTime.Now),
+                    Systemname = Environment.UserName.ToString()
+                };
+                AstroGlobal.Username = aUser.Username;
+                AstroGlobal.CanAddNew = aUser.CanAdd.Value;
+                AstroGlobal.CanModify = aUser.CanEdit.Value;
+                AstroGlobal.CanReport = aUser.CanReport.Value;
+                AstroGlobal.ActiveUserId = aUser.Sno;
+                AstroGlobal.Username = aUser.Username;
+                AstroGlobal.IsAdmin = aUser.Adminuser.Value;
+                new FrmNewKP().Show();
+                Hide();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Otp.");
+                this.Show();
+                return;
+            }
         }
     }
 }
