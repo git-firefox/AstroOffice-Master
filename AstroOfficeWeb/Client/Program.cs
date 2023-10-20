eb41e90be73ebe63a88fee25ad0bc370e1e39d98
mahesh-mobileartsme <mahesh.outsourcing@mobileartsme.com>
2023-10-20 16:50:04 +0530

    resolving cors error

--- AstroOfficeWeb/Client/Program.cs
+++ AstroOfficeWeb/Client/Program.cs
@@ -10,15 +10,16 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-if (builder.HostEnvironment.IsDevelopment())
-{
-    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-}
-else
-{
-    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://3.110.160.108/") });
-}
+//if (builder.HostEnvironment.IsDevelopment())
+//{
+//    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//}
+//else
+//{
+//    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://3.110.160.108/") });
+//}
 
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationStateService>();
