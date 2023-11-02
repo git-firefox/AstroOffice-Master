e5bafd73a3f083d94a9f403d17826a6838661822
Vikash <itsvikashshukla123@gmail.com>
2023-11-02 09:46:18 +0530

    added maui for mobile app

---
+++ AstroOfficeMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AstroOfficeMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ AstroOfficeMobile/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AstroOfficeMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
