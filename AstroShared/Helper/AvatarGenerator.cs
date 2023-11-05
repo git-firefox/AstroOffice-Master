f4f4826e80c155cecdcc619e96cf3ec4565c62ba
Sandip Rathva <sandip.rathva@outlook.in>
2023-11-05 23:54:56 +0530

    fix ui chnages

---
+++ AstroShared/Helper/AvatarGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstroShared.Helper
+{
+    public static class AvatarGenerator
+    {
+        public static string GenerateUserAvatar(string? username)
+        {
+            char firstLetter = !string.IsNullOrEmpty(username) ? username.ToUpper()[0] : 'U';
+
+            string svg = $@"<svg xmlns='http://www.w3.org/2000/svg' width='100' height='100'><circle cx='50' cy='50' r='40' fill='#007bff' /><text x='50%' y='50%' dominant-baseline='middle' text-anchor='middle' fill='white' font-size='36'>{firstLetter}</text></svg>";
+
+            byte[] svgBytes = System.Text.Encoding.UTF8.GetBytes(svg);
+            string base64Svg = Convert.ToBase64String(svgBytes);
+
+            return "data:image/svg+xml;base64," + base64Svg;
+        }
+    }
+}
