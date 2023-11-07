c3e86405d4b638d88ba5fe7af3a0b5918b1d5f52
Vikash <itsvikashshukla123@gmail.com>
2023-11-07 14:54:03 +0530

    designing of chart

---
+++ AstroOfficeMobile/Pages/Kundali.razor.cs
@@ -0,0 +1,19 @@
+﻿using AstroShared.Methods;
+using AstroShared.Models;
+using AstroShared.ViewModels;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstroOfficeMobile.Pages
+{
+    public partial class Kundali
+    {
+        private bool isNumVarshVisible = true;
+
+
+    }
+}
