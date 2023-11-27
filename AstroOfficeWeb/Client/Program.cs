bed62be14c9b888e5b8af6fe712f040188f864c3
admin <admin@TO28>
2023-11-27 13:22:23 +0530

    Added Mudblazor package

--- AstroOfficeWeb/Client/Program.cs
+++ AstroOfficeWeb/Client/Program.cs
@@ -5,11 +5,12 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddMudServices();
 //if (builder.HostEnvironment.IsDevelopment())
 //{
 //    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
