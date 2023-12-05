ba360edc470472569b07be28cd46ec4ef2822701
admin <admin@TO28>
2023-12-05 18:24:28 +0530

    creating list for manage category

--- AstroOfficeWeb/Client/Shared/CategoryDialog.razor.cs
+++ AstroOfficeWeb/Client/Shared/CategoryDialog.razor.cs
@@ -9,16 +9,28 @@ namespace AstroOfficeWeb.Client.Shared
         [CascadingParameter] MudDialogInstance? MudDialog { get; set; }
 
         [Parameter] public CategoryDialoge Category { get; set; } = new CategoryDialoge();
+       
 
 
+        private List<CategoryDialoge> CategoryList { get; set; } = new List<CategoryDialoge>();
         private void Cancel()
         {
             MudDialog!.Cancel();
         }
 
-        private void AddCategory()
+        public  void AddCategory()
         {
             //In a real world scenario this bool would probably be a service to delete the item from api/database
+            CategoryList.Add(new CategoryDialoge
+            {
+                Title = Category!.Title,
+                Slug = Category!.Slug,
+                 FileUpload = Category!.FileUpload,
+                ParentCategory = Category!.ParentCategory,
+                Description = Category!.Description,
+                Status = Category!.Status
+            });
+            
             Snackbar.Add("Category Added", Severity.Success);
             MudDialog!.Close(DialogResult.Ok(Category.Title));
         }
