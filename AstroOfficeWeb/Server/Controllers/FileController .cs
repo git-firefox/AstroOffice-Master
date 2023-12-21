9b0c061f43cdd730a2f7f6f54e3f44e03481afb7
Sandip Rathva <sandip.rathva@outlook.in>
2023-12-21 08:21:29 +0530

    save chnages

---
+++ AstroOfficeWeb/Server/Controllers/FileController .cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AstroOfficeWeb.Server.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        // POST api/<FileController>
+        [HttpPost]
+        public void Post([FromBody] object value)
+        {
+        }
+
+        // PUT api/<FileController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<FileController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
