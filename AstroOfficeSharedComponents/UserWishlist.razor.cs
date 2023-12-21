28e62b7214d0a1e57142dd81f9959c2cf9a71300
admin <admin@TO28>
2023-12-21 15:44:54 +0530

    added  view products, User wishlist, Cart, Orders pages

---
+++ AstroOfficeSharedComponents/UserWishlist.razor.cs
@@ -0,0 +1,55 @@
+﻿using AstroShared.DTOs;
+using AstroShared.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstroOfficeSharedComponents
+{
+    public partial class UserWishlist
+    {
+        private List<ViewProductDTO>? ViewProductDTOs { get; set; }
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            ViewProductDTOs = await ProductService.GetUserWishList();
+        }
+
+        private async Task OnCLick_ALickViewProdcut(ViewProductDTO productDTO)
+        {
+            //await LocalStorage.SetItemAsync<ViewProductDTO>(ApplicationConst.Local_SelectedProduct, productDTO);
+            NavigationManager.NavigateTo($"/product/{productDTO.Sno}/{productDTO.Name.Replace(" ", "-").ToLower()}");
+
+        }
+        private async Task OnCLick_ALickVRemoveWishlist(ViewProductDTO productDTO)
+        {
+
+            var result = await ProductService.IsDeletedFromWishList(productDTO.Sno);
+            if (result)
+            {
+                ViewProductDTOs?.Remove(productDTO);
+            }
+        }
+        private async Task OnCLick_BtnAddToCart(ViewProductDTO productDTO)
+        {
+            var tempQuantity = productDTO!.ProductQuantity + 1;
+            if (tempQuantity < 1 || tempQuantity > 5)
+            {
+                await JSRuntime.ShowToastAsync($"Unable to add more products to your cart. Limit exceeded.", SwalIcon.Error);
+                return;
+            }
+            var result = await ProductService.IsAddToCart(productDTO.Sno);
+            if (result)
+            {
+                productDTO!.ProductQuantity += 1;
+                await JSRuntime.ShowToastAsync($"{productDTO.Name} Added to your cart.", SwalIcon.Success);
+            }
+        }
+    }
+}
---
+++ AstroOfficeSharedComponents/ViewOrder.razor.cs
@@ -0,0 +1,31 @@
+﻿using AstroOfficeWeb.Shared.Models;
+using AstroShared.Utilities;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstroOfficeSharedComponents
+{
+    public partial class ViewOrder
+    {
+
+        [Parameter]
+        public long OrderId { get; set; }
+        public GetOrderResponse? Order { get; set; }
+        public CalculateOrderSummary? OrderSummary { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Order = await ProductService.GetUserOrder(OrderId);
+            OrderSummary = new(Order!.OrderItems.Sum(oi => oi.ProductQuantity * oi.ProductPrice));
+        }
+    }
+}
