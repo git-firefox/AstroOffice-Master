48155bac7b9342efe3c007e6658a787070f12124
git-firefox <sandip.rathva@outlook.com>
2023-12-23 12:29:37 +0530

    save chages

--- AstroOfficeWeb/Client/Pages/User/ManageUsers.razor.cs
+++ AstroOfficeWeb/Client/Pages/User/ManageUsers.razor.cs
@@ -1,6 +1,48 @@
-﻿namespace AstroOfficeWeb.Client.Pages.User
+﻿using System.Text.RegularExpressions;
+using MudBlazor;
+
+namespace AstroOfficeWeb.Client.Pages.User
 {
+    public class ManageUserViewModel
+    {
+
+    }
+
     public partial class ManageUsers
     {
+        private bool IsDrawerOpen { get; set; }
+        private bool IsSaveUserFormValid { get; set; }
+        void OpenDrawer(Anchor anchor)
+        {
+            IsDrawerOpen = !IsDrawerOpen;
+        }
+
+        string[] errors = { };
+        MudTextField<string> pwField1 = null!;
+        MudForm form = null!;
+
+        private IEnumerable<string> PasswordStrength(string pw)
+        {
+            if (string.IsNullOrWhiteSpace(pw))
+            {
+                yield return "Password is required!";
+                yield break;
+            }
+            if (pw.Length < 8)
+                yield return "Password must be at least of length 8";
+            if (!Regex.IsMatch(pw, @"[A-Z]"))
+                yield return "Password must contain at least one capital letter";
+            if (!Regex.IsMatch(pw, @"[a-z]"))
+                yield return "Password must contain at least one lowercase letter";
+            if (!Regex.IsMatch(pw, @"[0-9]"))
+                yield return "Password must contain at least one digit";
+        }
+
+        private string? PasswordMatch(string arg)
+        {
+            if (pwField1.Value != arg)
+                return "Passwords don't match";
+            return null;
+        }
     }
 }
