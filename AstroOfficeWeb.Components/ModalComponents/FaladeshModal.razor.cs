b24c73e52d3c61504cadf80d7e07da9be4c8148f
Sandip Rathva <sandip.rathva@outlook.in>
2024-01-03 09:23:56 +0530

    add faladehs model done with peding theming

---
+++ AstroOfficeWeb.Components/ModalComponents/FaladeshModal.razor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using MudBlazor;
+
+namespace AstroOfficeWeb.Components.ModalComponents
+{
+    public partial class FaladeshModal
+    {
+        [CascadingParameter] MudDialogInstance MudDialog { get; set; } = null!;
+        [Parameter] public string? BhavChalitSrc { get; set; }
+        [Parameter] public string? LaganSrc { get; set; }
+        [Parameter] public string? Kundali { get; set; }
+        [Parameter] public EventCallback<MouseEventArgs> OnClickPrint { get; set; }
+
+        void OnClick_BtnPrint(MouseEventArgs args)
+        {
+
+        }
+        void Cancel() => MudDialog.Cancel();
+    }
+}
--- AstroOfficeWeb/Client/Program.cs
+++ AstroOfficeWeb/Client/Program.cs
@@ -17,6 +17,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.RootComponents.Add<NavMenu>("#header-container");
 builder.RootComponents.Add<NavMenuMobile>("#footer-container");
 builder.Services.AddMudServices();
+builder.Services.AddMudBlazorDialog();
 builder.Services.AddScoped<ISnackbarService, CustomSnackbar>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredLocalStorage();
