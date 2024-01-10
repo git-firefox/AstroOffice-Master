ab2a2016abe59aca5e599df0da08589b320207a6
git-firefox <108627497+git-firefox@users.noreply.github.com>
2024-01-10 08:16:35 +0530

    fix file updlod bug

--- AstroOfficeWeb/Server/Models/MultipartFormRequest.cs
+++ AstroOfficeWeb/Server/Models/MultipartFormRequest.cs
@@ -1,13 +1,14 @@
 ﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace AstroOfficeWeb.Server.Models
 {
     public class MultipartFormRequest<TRequest>
     {
-        public JObject Data { get; set; } = default!;
+        public string Data { get; set; } = default!;
         public IFormFileCollection Files { get; set; } = default!;
 
-        public TRequest? GetDataObject() => this.Data.ToObject<TRequest>();
+        public TRequest? GetDataObject() => JsonConvert.DeserializeObject<TRequest>(this.Data);
     }
 }
